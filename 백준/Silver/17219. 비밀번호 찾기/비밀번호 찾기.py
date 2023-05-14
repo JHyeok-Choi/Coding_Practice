c3131686814f4e5a21a61e9ead577735c413214f
JHyeok-Choi <119499530+JHyeok-Choi@users.noreply.github.com>
2023-05-14 23:47:10 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 184 ms, Memory: 52372 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
@@ -5,7 +5,7 @@ N, M = map(int, inp().split())
 memo = {}
 
 for i in range(N):
-    address, password = map(str, inp().split())
+    address, password = inp().split()
     memo[address] = password
 
 sys.stdout.write("\n".join([memo[inp().rstrip()] for _ in range(M)]))
