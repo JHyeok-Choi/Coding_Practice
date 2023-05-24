d4c41c9efa352a9370a07f6ba1d0865e6dec199c
JHyeok-Choi <119499530+JHyeok-Choi@users.noreply.github.com>
2023-05-24 18:20:48 +0900

    [unrated] Title: 문자열 나누기, Time: 1.49 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.py
+++ 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.py
@@ -1,22 +1,15 @@
 def solution(s):
     answer = 0
-    x_num = 0
-    y_num = 0
-    x = s[0]
+    equal = 0
+    noteq = 0
     
-    for i, j in enumerate(s):
-        if x == j:
-            x_num += 1
-        else:
-            y_num += 1
-        if x_num == y_num:
+    for i in s:
+        if equal == noteq:
             answer += 1
-            if i+1 != len(s):
-                x = s[i+1]
+            x = i
+        if x == i:
+            equal += 1
         else:
-            if i == len(s)-1:
-                answer += 1
-    #if x == s[-1]:
-    #    answer += 1
+            noteq += 1
             
     return answer
