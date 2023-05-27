56b211abd27528ca1d8e5493486a316b0e43435c
JHyeok-Choi <119499530+JHyeok-Choi@users.noreply.github.com>
2023-05-27 22:22:10 +0900

    Update 나부 함대 데이터.py
    Add Annotation.

--- 백준/Bronze/9654. 나부 함대 데이터/나부 함대 데이터.py
+++ 백준/Bronze/9654. 나부 함대 데이터/나부 함대 데이터.py
@@ -4,3 +4,12 @@ J-Type 327     Light Combat   Unlimited  1
 NX Cruiser     Medium Fighter Limited    18        
 N1 Starfighter Medium Fighter Unlimited  25        
 Royal Cruiser  Light Combat   Limited    4          """)
+
+"""a = [["SHIP NAME", "CLASS", "DEPLOYMENT", "IN SERVICE"],
+     ["N2 Bomber", "Heavy Fighter", "Limited", 21],
+     ["J-Type 327", "Light Combat", "Unlimited", 1],
+     ["NX Cruiser", "Medium Fighter", "Limited", 18],
+     ["N1 Starfighter", "Medium Fighter", "Unlimited", 25],
+     ["Royal Cruiser", "Light Combat", "Limited", 4]]
+for n, (i, j, k ,l) in enumerate(a):
+    print('%-14s' % f"{i}", '%-14s' % f"{j}", '%-10s' % f"{k}", '%-9s' % f"{l}")"""
