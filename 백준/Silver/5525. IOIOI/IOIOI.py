2b46746049be24f0c097e2e9f7805090722dc4b8
JHyeok-Choi <119499530+JHyeok-Choi@users.noreply.github.com>
2023-06-12 22:36:56 +0900

    [Silver I] Title: IOIOI, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,16 +1,13 @@
 N = int(input())
 M = int(input())
 S = input()
-count, repeat, i = 0, 0, 0
+count = 0
 
 text = 'IO' * N + 'I'  # PN string
 tl = len(text)  # PN len
 
-while i + tl <= M:
-    if text == S[i:i+(tl)]:  # PN있으면 count + 1, i + 2가 되게
-        count += 1           # i+1은 I 다음인 O 라서 볼 필요 없습니다. 
-        i += 2
-    else:
-        i += 1
+for i in range(M-(tl) + 1):  # PN길이만큼씩 탐색
+    if text == S[i:i+(tl)]:  # PN있으면 count + 1
+        count += 1
 
 print(count)
