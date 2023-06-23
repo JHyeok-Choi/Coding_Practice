72ef985865a19ce1dbd74488cd9354ef91c98d53
JHyeok-Choi <119499530+JHyeok-Choi@users.noreply.github.com>
2023-06-23 17:20:32 +0900

    [Silver III] Title: 바이러스, Time: 64 ms, Memory: 34128 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,23 +1,26 @@
 import sys
+from collections import deque
 inp = sys.stdin.readline
 
-n = int(inp())
-m = int(inp())
+n = int(inp())  # 컴퓨터의 수.
+m = int(inp())  # 컴퓨터 쌍의 수.
 
-graph = [[] for i in range(n+1)]
-wait = [1]
-count = set()
+graph = [[] for _ in range(n+1)]  # 그래프 틀.
+infected = [0] * (n+1)  # 컴퓨터에 감염여부 체크.
 
 for i in range(m):
     x, y = map(int, inp().split())
-    graph[x] += [y]
-    graph[y] += [x]
+    graph[x] += [y]  # x에 y를
+    graph[y] += [x]  # y에 x를 연결합니다.
 
-while wait:
-    a = wait.pop(0)
-    for i in graph[a]:
-        if i not in count:
-            wait.append(i)
-            count.add(i)
+infected[1] = 1  # 1번 컴퓨터에서 시작하니 체크.
+Q = deque([1])
 
-print(len(count)-1)
+while Q:
+    z = Q.popleft()
+    for j in graph[z]:
+        if infected[j] == 0:  # 감염되지 않았다면 감염된걸로 체크.
+            Q.append(j)
+            infected[j] = 1
+
+print(sum(infected)-1)
