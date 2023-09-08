c6c7552a9f3cc8ff7d8786b20f89d44f30d36fc9
JHyeok-Choi <119499530+JHyeok-Choi@users.noreply.github.com>
2023-09-09 00:23:37 +0900

    [Bronze II] Title: 시험 감독, Time: 612 ms, Memory: 150704 KB -BaekjoonHub

--- 백준/Bronze/13458. 시험 감독/시험 감독.py
+++ 백준/Bronze/13458. 시험 감독/시험 감독.py
@@ -1,15 +1,14 @@
 N = int(input())
 A = list(map(int, input().split()))
 B, C = map(int, input().split())
-count = 0
+count = N
 
 for i in A:
     i -= B
-    count += 1
     if i > 0:
-        count += i // C
-        i %= C
-    if i > 0:
-        count += 1
+        if i % C:
+            count += (i // C) + 1
+        else:
+            count += i // C
 
 print(count)
