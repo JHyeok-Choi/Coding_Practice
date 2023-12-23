9287a7a37086c97cb239b9b78c2a960f014590a3
JHyeok-Choi <119499530+JHyeok-Choi@users.noreply.github.com>
2023-12-23 09:28:03 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.py
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.py
@@ -1,46 +1,31 @@
 import sys
 
-input = sys.stdin.readline
+def password_check():
+    while True:
+        test_case = sys.stdin.readline().rstrip()
+        if test_case == 'end':
+            break
+        
+        vowel = ['a', 'e', 'i', 'o', 'u']
+        vowel_chk = False
 
-consonant = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
-vowel = ['a', 'e', 'i', 'o', 'u']
-
-
-while True:
-    consonant_cnt = 0
-    vowel_cnt = 0
-    test_case = input().rstrip()
-
-    if test_case == "end":
-        break
-    else:
-        consonant_stk = 0
-        vowel_stk = 0
-        last = 'aa'
-
-        for idx, alpha in enumerate(test_case):
-            if alpha in consonant:
-                consonant_cnt += 1
-                consonant_stk += 1
-                vowel_stk = 0
-            else:
-                vowel_cnt += 1
-                vowel_stk += 1
-                consonant_stk = 0
-            
-            if (consonant_stk or vowel_stk) >= 3:
-                print(f"<{test_case}> is not acceptable.")
+        for i in range(len(test_case)):
+            if test_case[i] in vowel:
+                vowel_chk = True
+            if i > 0 and test_case[i] == test_case[i - 1] and test_case[i] != 'e' and test_case[i] != 'o':
+                vowel_chk = False
                 break
+            if i > 1 and test_case[i] in vowel and test_case[i - 1] in vowel and test_case[i - 2] in vowel:
+                vowel_chk = False
+                break
+            elif i > 1 and test_case[i] not in vowel and test_case[i - 1] not in vowel and test_case[i - 2] not in vowel:
+                vowel_chk = False
+                break
+        
+        if vowel_chk:
+            print(f"<{test_case}> is acceptable.")
+        else:
+            print(f"<{test_case}> is not acceptable.")
 
-            if last == alpha:
-                if last == 'e' or last == 'o':
-                    pass
-                else:
-                    print(f"<{test_case}> is not acceptable.")
-                    break
-            last = alpha
-            if idx == len(test_case) - 1:
-                if vowel_cnt == 0:
-                    print(f"<{test_case}> is not acceptable.")
-                else:
-                    print(f"<{test_case}> is acceptable.")
+
+password_check()
