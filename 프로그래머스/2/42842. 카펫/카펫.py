ad3a69cc628d7295f98c846fa843effba36b441c
JHyeok-Choi <119499530+JHyeok-Choi@users.noreply.github.com>
2024-01-09 01:17:06 +0900

    [level 2] Title: 카펫, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -7,19 +7,12 @@ def solution(brown, yellow):
     
     hPoR = (brown + 4) // 2
     wh = brown + yellow
-    
-    def calcul_h(a):
-        h = (brown + yellow) // a
-        return h
         
     for i in range(3, hPoR):
-        w = calcul_h(i)
+        w = wh // i
+        
         if i * w == wh and (i - 2) * (w - 2) == yellow:
-            if calcul_h(i) < i:
-                continue
-            else:
-                print(i)
-                answer = [w, i]
-                break
+            answer = [w, i]
+            break
         
     return answer
