646d02e2ee8f9d9894e7d6f9e83013113b28a743
JHyeok-Choi <119499530+JHyeok-Choi@users.noreply.github.com>
2024-01-13 00:10:20 +0900

    [level Lv.2] Title: 연속 부분 수열 합의 개수, Time: 4242.43 ms, Memory: 43.6 MB -BaekjoonHub

--- 프로그래머스/Lv.2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/Lv.2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,5 +1,4 @@
 def solution(elements):    
-    loop = 2 * elements
-    table = set(sum(loop[i: i + j]) for j in range(len(elements)) for i in range(len(elements)))
+    loop = elements * 2
     
-    return len(table)
+    return len(set(sum(loop[i: i + j]) for j in range(len(elements)) for i in range(len(elements))))
