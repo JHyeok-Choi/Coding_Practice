5cb466ad683c9d603b7d4f91e1b8820f8c0f9652
JHyeok-Choi <119499530+JHyeok-Choi@users.noreply.github.com>
2024-01-21 00:10:19 +0900

    Update 기능개발.py
    [level 2] Title: 기능개발, Time: 0.03 ms, Memory: 9.97 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,19 +1,9 @@
-from math import ceil
-
 def solution(progresses, speeds):
     answer = []
-    days = [ceil((100 - p )/ s) for p, s in zip(progresses, speeds)]
-    count = 0
-    current = days[0]
+    for p, s in zip(progresses, speeds):
+        if len(answer) == 0 or answer[-1][0] < -((p - 100) // s):
+            answer.append([-((p - 100) // s), 1])
+        else:
+            answer[-1][1] += 1
     
-    for i in range(len(progresses)):
-        if current < days[i]:
-            answer.append(count)
-            current = days[i]
-            count = 0
-        
-        count += 1
-
-    answer.append(count)
-    
-    return answer
+    return [a[1] for a in answer]
