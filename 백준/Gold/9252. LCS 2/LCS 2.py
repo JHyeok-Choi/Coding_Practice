64b3d69d48f005c97cefd49ce4e4f68d3f3613bf
JHyeok-Choi <119499530+JHyeok-Choi@users.noreply.github.com>
2024-02-07 00:27:12 +0900

    [Gold IV] Title: LCS 2, Time: 348 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.py
+++ 백준/Gold/9252. LCS 2/LCS 2.py
@@ -1,19 +1,14 @@
-import sys
-input = sys.stdin.readline
+a, b = input(), input()
+a_l, b_l = len(a), len(b)
+dp = [''] * b_l
 
-A = [""] + list(input().rstrip())
-B = [""] + list(input().rstrip())
-LCS = [[""]*len(B) for _ in range(len(A))]
+for i in range(a_l):
+    lcs = ''
+    for j in range(b_l):
+        if len(lcs) < len(dp[j]):
+            lcs = dp[j]
+        elif a[i] == b[j]:
+            dp[j] = lcs + a[i]
 
-for i in range(1, len(A)):
-    for j in range(1, len(B)):
-        if A[i] == B[j]:
-            LCS[i][j] = LCS[i-1][j-1] + A[i]
-        else:
-            if len(LCS[i-1][j]) >= len(LCS[i][j-1]):
-                LCS[i][j] = LCS[i-1][j]
-            else:
-                LCS[i][j] = LCS[i][j-1]
-
-result = LCS[-1][-1]
-print(len(result), result, sep="\n")
+print(len(sorted(dp, key=len)[-1]))
+print(sorted(dp, key=len)[-1])
