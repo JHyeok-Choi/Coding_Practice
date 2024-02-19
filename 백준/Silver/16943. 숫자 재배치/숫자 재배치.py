d79708cc7404b6b48b97f68ee7b5d26f2e1adf9b
JHyeok-Choi <119499530+JHyeok-Choi@users.noreply.github.com>
2024-02-20 00:51:29 +0900

    [Silver I] Title: 숫자 재배치, Time: 248 ms, Memory: 56504 KB -BaekjoonHub

--- 백준/Silver/16943. 숫자 재배치/숫자 재배치.py
+++ 백준/Silver/16943. 숫자 재배치/숫자 재배치.py
@@ -2,12 +2,9 @@ import itertools
 
 A, B = map(int, input().split())
 C = -1
-table = [''.join(i) for i in itertools.permutations(str(A))]
+table = [''.join(i) for i in itertools.permutations(str(A)) if i[0] is not '0']
 
 for i in table:
-    if i[0] == '0':
-        continue
-
     i = int(i)
 
     if i < B:
