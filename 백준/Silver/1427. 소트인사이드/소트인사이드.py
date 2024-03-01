385fc548dae2d921cb8a66aecb4e06cc6a724b5a
JHyeok-Choi <119499530+JHyeok-Choi@users.noreply.github.com>
2024-03-02 00:07:44 +0900

    [Silver V] Title: 소트인사이드, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.py
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.py
@@ -1,10 +1,6 @@
 N = int(input())
- 
-table = []
-for i in str(N):
-    table.append(int(i))
-    
+
+table = list(map(int, str(N)))
 table.sort(reverse=True)
- 
-for i in table:
-    print(i, end='')
+
+print(*table, sep='')
