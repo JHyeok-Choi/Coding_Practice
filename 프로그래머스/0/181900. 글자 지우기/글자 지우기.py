91d7dce02648d17a52265ec41712e25484e48f66
JHyeok-Choi <119499530+JHyeok-Choi@users.noreply.github.com>
2024-04-02 20:22:53 +0900

    [level 0] Title: 글자 지우기, Time: 0.07 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.py
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.py
@@ -1,8 +1,3 @@
 def solution(my_string, indices):
     answer = ''
-    indices.sort()
-    for i in range(len(my_string)):
-        if i not in indices:
-            answer += my_string[i]
-            
     return answer
