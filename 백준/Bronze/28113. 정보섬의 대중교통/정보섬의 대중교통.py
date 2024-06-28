82d3ae7354c6fd49de0f5e40459ad6acad135122
JHyeok-Choi <119499530+JHyeok-Choi@users.noreply.github.com>
2024-06-29 08:58:54 +0900

    [Bronze V] Title: 정보섬의 대중교통, Time: 32 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Bronze/28113. 정보섬의 대중교통/정보섬의 대중교통.py
+++ 백준/Bronze/28113. 정보섬의 대중교통/정보섬의 대중교통.py
@@ -1,3 +1,2 @@
-N, A, B = map(int, input().split())
-C = B if B > N else N
-print("Subway" if C < A else "Anything" if C == A else "Bus")
+_,A,B=map(int,input().split())
+print(("Anything","Subway","Bus")[(A>B)-(A<B)])
