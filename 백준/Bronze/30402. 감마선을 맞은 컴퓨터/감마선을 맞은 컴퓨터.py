58c0f37b0d49306e836b0d736236cb5b72ca27e3
JHyeok-Choi <119499530+JHyeok-Choi@users.noreply.github.com>
2024-07-18 23:11:50 +0900

    [Bronze IV] Title: 감마선을 맞은 컴퓨터, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30402. 감마선을 맞은 컴퓨터/감마선을 맞은 컴퓨터.py
@@ -0,0 +1,11 @@
+for i in range(15):
+    j = input().split()
+    if "w" in j:
+        print("chunbae")
+        break
+    elif "b" in j:
+        print("nabi")
+        break
+    elif "g" in j:
+        print("yeongcheol")
+        break
