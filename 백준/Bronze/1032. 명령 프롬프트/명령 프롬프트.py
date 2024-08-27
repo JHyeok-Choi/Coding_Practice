fd404564f3c626558ef23efa129f6600405d17c7
JHyeok-Choi <119499530+JHyeok-Choi@users.noreply.github.com>
2024-08-27 23:57:50 +0900

    [Bronze I] Title: 명령 프롬프트, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.py
+++ 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.py
@@ -1,3 +1,2 @@
 filenames = [input() for i in range(int(input()))]
-answer = [list(j)[0] if len(j) <= 1 else '?' for j in (set(map(lambda x:x[i], filenames)) for i in range(len(filenames[0])))]
-print(''.join(answer))
+print(''.join([list(j)[0] if len(j) <= 1 else '?' for j in (set(map(lambda x:x[i], filenames)) for i in range(len(filenames[0])))]))
