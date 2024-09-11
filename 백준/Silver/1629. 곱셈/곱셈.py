fe5edd41b9dc363798d0306ff96612a3611ed9b4
JHyeok-Choi <119499530+JHyeok-Choi@users.noreply.github.com>
2024-09-11 23:48:17 +0900

    [Silver I] Title: 곱셈, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -1,1 +1,19 @@
 print(pow(*map(int, input().split())))
+
+# A, B, C = map(int, input().split())
+
+# # A^B % C => (A^(B/2) * A^(B/2)) % C      (if B는 짝수)
+# #         => (A^(B/2) * A^(B/2) * A) % C  (if B는 홀수)  
+
+# def mul(A, B, C):
+#     if B == 1:
+#         return(A % C)
+    
+#     num = mul(A, B // 2, C)
+
+#     if B % 2:
+#         return (num ** 2 % C * A) % C
+#     else:
+#         return (num ** 2) % C
+
+# print(mul(A, B, C))
