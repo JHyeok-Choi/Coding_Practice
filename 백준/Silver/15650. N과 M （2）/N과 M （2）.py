eb79bb73c6ccd9dc82416a8c8dcc9a885c3825d8
JHyeok-Choi <119499530+JHyeok-Choi@users.noreply.github.com>
2024-09-14 23:45:45 +0900

    [Silver III] Title: N과 M (2), Time: 28 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -2,5 +2,5 @@ from itertools import combinations
 
 N, M = map(int, input().split())
 
-for i in combinations([i for i in range(1, N + 1)], M):
+for i in combinations(range(1, N + 1), M):
     print(*i)
