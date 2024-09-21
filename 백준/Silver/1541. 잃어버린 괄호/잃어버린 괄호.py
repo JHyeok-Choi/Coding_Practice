747ce78acd1ecedaafcfad437114a8d1cb174287
JHyeok-Choi <119499530+JHyeok-Choi@users.noreply.github.com>
2024-09-21 23:47:09 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 32 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,2 +1,2 @@
-table = list(map(lambda x: sum(map(int, x.split('+'))), input().split('-')))
-print(table[0] - sum(table[1:]))
+n, *m = list(map(lambda x: sum(map(int, x.split('+'))), input().split('-')))
+print(n - sum(m))
