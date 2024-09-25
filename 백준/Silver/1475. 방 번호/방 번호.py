f076bdc3b8c16bd3509c28a8ed4fb0e1e52b401b
JHyeok-Choi <119499530+JHyeok-Choi@users.noreply.github.com>
2024-09-26 00:04:10 +0900

    [Silver V] Title: 방 번호, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.py
+++ 백준/Silver/1475. 방 번호/방 번호.py
@@ -1,13 +1,13 @@
 N = input()
-table = {'0': 0, '1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0}
+table = {i: 0 for i in range(10)}
 
 for i in N:
     if i == '6' or i == '9':
-        if table['6'] <= table['9']:
-            table['6'] += 1
+        if table[6] <= table[9]:
+            table[6] += 1
         else:
-            table['9'] += 1
+            table[9] += 1
     else:
-        table[i] += 1
+        table[int(i)] += 1
 
 print(max(table.values()))
