16926ad56f53b6f5c461076729320d2eee98a895
JHyeok-Choi <119499530+JHyeok-Choi@users.noreply.github.com>
2024-09-26 00:34:22 +0900

    [Silver V] Title: 방 번호, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.py
+++ 백준/Silver/1475. 방 번호/방 번호.py
@@ -2,12 +2,11 @@ N = map(int, input())
 table = {i:0 for i in range(10)}
 
 for i in N:
-    if i == 6 or i == 9:
-        if table[6] <= table[9]:
-            table[6] += 1
-        else:
-            table[9] += 1
+    if i == 9:
+        table[6] += 1
     else:
         table[i] += 1
+
+table[6] = (table[6] + 1) // 2
 
 print(max(table.values()))
