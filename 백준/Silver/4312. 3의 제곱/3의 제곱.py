f34c59f06c0cf5c49d2540506f5ebfbc4eb7c35b
JHyeok-Choi <119499530+JHyeok-Choi@users.noreply.github.com>
2024-10-09 23:52:46 +0900

    [Silver III] Title: 3의 제곱, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/4312. 3의 제곱/3의 제곱.py
@@ -0,0 +1,12 @@
+import sys
+
+S = list(map(int, sys.stdin.read().split()))[:-1]
+s = s = [3 ** i for i in range(100)]
+
+for i in S:
+    strS = bin(i - 1)[2:][::-1]
+    table = [s[j] for j in list(filter(lambda x: strS[x] == '1', range(len(strS))))]
+    if len(table) == 0:
+        print('{ }')
+    else:
+        print('{ ' + str(table)[1:-1] + ' }')
